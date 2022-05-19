a49ddee970a406e8137c0d8fcba9e5a01554639b
pryme <akexboswell8@gmail.com>
2022-05-19 15:21:37 -0400

    Initial commit

---
+++ src/main/java/com/alex/tacostand/TacoStandApplication.java
@@ -0,0 +1,13 @@
+package com.alex.tacostand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoStandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoStandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alex/tacostand/TacoStandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.tacostand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoStandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
